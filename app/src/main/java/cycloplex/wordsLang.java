472ba32bd30e4a2cc0ec0f89c8f88b0871ca31ae
icarofreire <icarofre99@gmail.com>
2024-05-03 09:47:03 -0300

    inserido parenteses para contagem de complexidades;

--- app/src/main/java/cycloplex/wordsLang.java
+++ app/src/main/java/cycloplex/wordsLang.java
@@ -24,16 +24,16 @@ public class wordsLang {
     public static final HashMap<String, String[]> langWords = new HashMap<String, String[]>() {
         {
             /**\/ ;*/
-            String[] keywords = {"if","else","while","case","for","switch","do","continue","break","&&","||","?",":","catch","finally","throw","throws","default","return"};
+            String[] keywords = {"if","else","while","case","for","switch","do","continue","break","&&","||","?",":","catch","finally","throw","throws","default","return", "(", ")"};
             put("java", keywords);
 
-            keywords = new String[]{"if","else","while","case","for","switch","do","continue","break","&&","||","?",":","catch","finally","throw","throws","default","return"};
+            keywords = new String[]{"if","else","while","case","for","switch","do","continue","break","&&","||","?",":","catch","finally","throw","throws","default","return", "(", ")"};
             put("js", keywords);
 
-            keywords = new String[]{"if","else","while","case","for","switch","do","continue","break","&&","||","?",":","catch","finally","throw","throws","default","return"};
+            keywords = new String[]{"if","else","while","case","for","switch","do","continue","break","&&","||","?",":","catch","finally","throw","throws","default","return", "(", ")"};
             put("tsx", keywords);
 
-            keywords = new String[]{"if","else", "elif","while","case","for","switch","do","continue","break","and","or","?",":","catch","finally","throw","throws","default","return", "except"};
+            keywords = new String[]{"if","else", "elif","while","case","for","switch","do","continue","break","and","or","?",":","catch","finally","throw","throws","default","return", "except", "(", ")"};
             put("py", keywords);
         }
     };
